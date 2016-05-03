2442f1acc842a518ad47a04e287aa783ca1d2e6a
sunyu <584410221@qq.com>
2016-05-03 18:39:54 +0800

    接入了tabbar，目前出了些问题，tabbar存在时，listview无法滚动

--- index.ios.js
+++ index.ios.js
@@ -4,6 +4,8 @@
  * @flow
  */
 
+//var Message = require('Message')
+
 import React, {
     AppRegistry,
     Component,
@@ -14,35 +16,126 @@ import React, {
     NavigatorIOS,
     TouchableHighlight,
     Image,
+    TabBarIOS,
 } from 'react-native';
 
 
 var SYNavigator = React.createClass ({
+    getInitialState: function () {
+        return {
+            selectedTab: 'Message'
+        }
+    },
+    selectTab: function (tab) {
+        this.setState({
+            selectedTab: tab
+        })
+    },
     render : function () {
+        return(
+             <TabBarIOS tintColor="#45b640" barTintColor="#F5F6F8">
+                <TabBarIOS.Item
+                    icon = {require('./resource/message.png')}
+                    selectedIcon = {require('./resource/message_fill.png')}
+                    onPress = {() => this.selectedTab('Message')}
+                    title = '新闻'
+                    selected = {this.state.selectedTab === 'Message'}
+                >
+                    {this.addNavigator(Message, '新闻', require('./resource/reply.png'))}
+                </TabBarIOS.Item>
+                <TabBarIOS.Item
+                    icon = {require('./resource/contact.png')}
+                    selectedIcon = {require('./resource/contact_fill.png')}
+                    onPress = {() => this.selectedTab('Contact')}
+                    title = '融资'
+                    selected = {this.state.selectedTab === 'Message'}
+                >
+                    {this.addNavigator(Message, '融资', require('./resource/reply.png'))}
+                </TabBarIOS.Item>
+                <TabBarIOS.Item
+                    icon = {require('./resource/found.png')}
+                    selectedIcon = {require('./resource/found_fill.png')}
+                    onPress = {() => this.selectedTab('Found')}
+                    title = '众筹'
+                    selected = {this.state.selectedTab === 'Message'}
+                >
+                    {this.addNavigator(Message, '众筹', require('./resource/reply.png'))}
+                </TabBarIOS.Item>
+                <TabBarIOS.Item
+                    icon = {require('./resource/me.png')}
+                    selectedIcon = {require('./resource/me_fill.png')}
+                    onPress = {() => this.selectedTab('Me')}
+                    title = '消息'
+                    selected = {this.state.selectedTab === 'Message'}
+                >
+                    {this.addNavigator(Message, '消息', require('./resource/reply.png'))}
+                </TabBarIOS.Item>
+            </TabBarIOS>
+        )
+    },
+    addNavigator: function (component, title, rightBottonIcon) {
         return(
             <NavigatorIOS
                 style={styles.container}
                 initialRoute = {{
-                            title: '新闻',
-                            component: SYListView,
-                        }}
+                    title: title,
+                    component: component,
+                }}
+                rightBottonIcon = {rightBottonIcon}
                 configureScene={() => {
-                                return Navigator.SceneConfigs.VerticalDownSwipeJump;
-                            }}
+                    return Navigator.SceneConfigs.VerticalDownSwipeJump;
+                }}
                 renderScene={(route, navigator) => {
-                            let Component = route.component;
-                            if(route.component) {
-                                return <Component {...route.params} navigator={navigator} />
-                            }
-                        }}/>
+                    let Component = route.component;
+                    if(route.component) {
+                        return <Component {...route.params} navigator={navigator} />
+                    }
+                }}
+            />
         );
     }
 });
 
-var SYListView = React.createClass ({
+var Message = React.createClass ({
     getInitialState: function() {
         var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
         var rowInfo = [{
+                author: 'Samsun',
+                authorIcon: 'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=2044316434,1110760480&fm=116&gp=0.jpg',
+                time: '27分钟前',
+                category: '大公司',
+                title: 'UC推出新媒体平台，阿里再次布局内容生态',
+                commentNum: '1115',
+                contentImage: 'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2652453787,3908676136&fm=116&gp=0.jpg'
+            },
+            {
+                author: 'Echo',
+                authorIcon: 'https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=2264218307,3618225719&fm=116&gp=0.jpg',
+                time: '1小时前',
+                category: '早期项目',
+                title: 'P2P模式的租车公司Drivy获3500万美元融资',
+                commentNum: '0',
+                contentImage: 'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1843325743,3380403034&fm=116&gp=0.jpg'
+            },
+            {
+                author: 'Samsun',
+                authorIcon: 'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=2044316434,1110760480&fm=116&gp=0.jpg',
+                time: '27分钟前',
+                category: '大公司',
+                title: 'UC推出新媒体平台，阿里再次布局内容生态',
+                commentNum: '1115',
+                contentImage: 'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2652453787,3908676136&fm=116&gp=0.jpg'
+            },
+            {
+                author: 'Echo',
+                authorIcon: 'https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=2264218307,3618225719&fm=116&gp=0.jpg',
+                time: '1小时前',
+                category: '早期项目',
+                title: 'P2P模式的租车公司Drivy获3500万美元融资',
+                commentNum: '0',
+                contentImage: 'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1843325743,3380403034&fm=116&gp=0.jpg'
+            },
+            {
                 author: 'Samsun',
                 authorIcon: 'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=2044316434,1110760480&fm=116&gp=0.jpg',
                 time: '27分钟前',
@@ -71,6 +164,7 @@ var SYListView = React.createClass ({
                 dataSource={this.state.dataSource}
                 renderRow={(rowData) =>this.renderRow(rowData)}
             />
+
         );
     },
     renderRow: function (rowData:string) {
@@ -83,10 +177,14 @@ var SYListView = React.createClass ({
                             source={{uri:rowData.authorIcon}}/>
                         <Text style={styles.listViewNameTime}>{rowData.author}</Text>
                         <Text style={styles.listViewNameTime}>{rowData.time}</Text>
+                        <View style={styles.listViewCategory}>
+                            <Text style={styles.listViewCategoryName}>{rowData.category}</Text>
+                            <View style={styles.listViewTag}></View>
+                        </View>
                     </View>
                     <View style={styles.listViewBottom}>
                         <View style={styles.listViewBottom_titleView}>
-                            <Text style={styles.header}>{rowData.title}</Text>
+                            <Text style={styles.listViewBottom_title}>{rowData.title}</Text>
                             <View style = {styles.listViewBottom_commentView}>
                                 <Image
                                     style = {styles.listViewBottom_commentImage}
@@ -100,6 +198,7 @@ var SYListView = React.createClass ({
                                 source={{uri:rowData.contentImage}}/>
                         </View>
                     </View>
+                    <View style={styles.seperator}></View>
                 </View>
             </TouchableHighlight>
         );
@@ -119,7 +218,7 @@ var SYListView = React.createClass ({
 var SYSconedPage = React.createClass({
     render:function() {
         return(
-            <SYListView />
+            <Message />
         );
     }
 });
@@ -140,7 +239,7 @@ var styles = StyleSheet.create({
     listViewTop: {
         height: 40,
         flexDirection: 'row',
-        //alignItems: 'center',
+        justifyContent: 'center',
     },
     listViewNameTime: {
         marginTop: 10,
@@ -149,7 +248,26 @@ var styles = StyleSheet.create({
         fontSize: 12,
         color: 'gray',
         paddingTop: 4,
-        paddingBottom: 4,
+    },
+    listViewCategory: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'flex-end',
+        marginRight: 10,
+    },
+    listViewCategoryName: {
+        marginTop: 10,
+        marginRight: 5,
+        fontSize: 12,
+        height: 20,
+        paddingTop: 4,
+        color: 'purple',
+    },
+    listViewTag: {
+        marginTop: 14,
+        backgroundColor: 'red',
+        width: 3,
+        height: 13,
     },
     listViewTop_icon: {
         width: 20,
@@ -158,6 +276,7 @@ var styles = StyleSheet.create({
         marginTop: 10,
     },
     listViewBottom: {
+        flex: 1,
         flexDirection: 'row',
     },
     listViewBottom_titleView: {
@@ -189,6 +308,10 @@ var styles = StyleSheet.create({
     },
     listViewBottom_comment: {
 
+    },
+    seperator: {
+        height: 0.5,
+        backgroundColor: '#d0d0d0',
     }
 });
 
